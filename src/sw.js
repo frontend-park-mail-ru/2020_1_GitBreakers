162e6908d96439c9e4dd6d2b68f785adc8b824ae
AntonElagin <AntonElagin@users.noreply.github.com>
2020-04-18 21:02:34 +0300

    delete 'offline' redirect

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-restricted-globals */
 const CACHE_NAME = 'myCache';
 
 const cacheUrls = [
-  // '/profile/',
   'index.html',
   'main.css',
   'main.js',
@@ -10,22 +10,18 @@ const cacheUrls = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)// CACHE_NAME)
-      .then((cache) =>
-        cache.addAll(cacheUrls)),
+      .then((cache) => cache.addAll(cacheUrls)),
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((resp) => {
-        return resp || fetch(event.request).then((response) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
-      })
-      .catch(() => return caches.match)
+      .then((resp) => resp || fetch(event.request).then((response) => caches
+        .open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        })))
+      // .catch(() => //caches.match('/offline')),
   );
 });
